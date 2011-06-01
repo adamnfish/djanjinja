72fddfd1caa6d68e6a19cab2331fd0459acb7f27
Adam Fisher <adam.fisher@tobias.tv>
2011-06-01 14:14:14 +0100

    Added test file to hold RequestFactory class. Using Django's RequestFactory for tests doesn't work too well because Djanjinja's middleware isn't run. Djanjinja now includes a modified RequestFactory that extends the request object with Djanjinja's Context.
    Added .gitignore file to mirror the existing .hgignore.

---
+++ djanjinja/test.py
@@ -0,0 +1,27 @@
+"""
+Test utils for Djanjinja.
+"""
+
+from django.test.client import RequestFactory as DjangoRequestFactory
+
+from djanjinja.middleware import RequestContextMiddleware
+
+
+class RequestFactory(DjangoRequestFactory):
+    """
+    Wraps Django's RequestFactory. This class adds Djanjinja's Context
+    class to the request object so that views using the
+    request.Context({*}).render_* pattern can be tested using
+    RequestFactories.
+    """
+    def request(self, *args, **kwargs):
+        """
+        Wrap the default request method. We'll create the WSGIRequest
+        object in Django's RequestFactory and extend it here with
+        Djanjinja's Context.
+        """
+        # call request on parent class
+        request = super(RequestFactory, self).request(*args, **kwargs)
+        # mutate the request object using Djanjinja's middleware
+        RequestContextMiddleware.process_request(request)
+        return request
